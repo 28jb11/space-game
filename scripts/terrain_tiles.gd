41e2d0c688008b7b0713daba7f6ea0d9fb094f06
James Bowman <jabowman@proton.me>
2024-12-05 11:15:25 +1100

    creating marching squares data structure for terrain tilesets

--- scripts/terrain_tiles.gd
+++ scripts/terrain_tiles.gd
@@ -2,11 +2,53 @@ extends TileMapLayer
 
 var tileset : TileSet
 
-func initialize():
-	tileset = self.tile_set
-	for i in range(tileset.get_source_count()):
-		print(tileset.get_source(i))
 
+
+func initialize():
+	# get tileset
+	# tileset has multiple sources. each source is a different terrain set
+	tileset = self.tile_set
+	#test_tileset_sources(tileset)
+	
+	# create structure for each terrain source
+	var tile_sources = []
+	for source_id in range(tileset.get_source_count()):
+		var ms = []
+		for i in range(16):
+			ms.append([])
+
+		var atlas_size = tileset.get_source(source_id).get_atlas_grid_size()
+		for x in range(atlas_size.x):
+			for y in range(atlas_size.y):
+				var ms_case = tileset.get_source(source_id).get_tile_data(Vector2i(x , y), 0).get_custom_data("ms_case")
+				if ms_case < 16:
+					ms[ms_case].append(Vector2i(x,y))
+		tile_sources.append(ms)
+		print(tile_sources)
+		
+
+func test_tileset_sources(tileset: TileSet):
+	
+	var y_offset = 0
+	# for every tileset source in the tileset
+	for source_id in range(tileset.get_source_count()):
+		# initialize source to a variable
+		var tileset_source = tileset.get_source(source_id)
+		
+		var atlas_size = tileset_source.get_atlas_grid_size()
+		print(atlas_size)
+	
+		for x in range(atlas_size.x):
+			for y in range(atlas_size.y):
+				var tile_data = tileset_source.get_tile_data(Vector2i(x , y), 0)
+				print(tile_data.get_custom_data("ms_case"))
+				
+				set_cell(Vector2i(x , y + y_offset), (source_id), Vector2i(x , y), 0)
+				print(
+				"setting cell at: ", x, " , ", (y + y_offset),
+				" | source_id: ", source_id,
+				" | atlas position: ", x, " , ", y)
+		y_offset += atlas_size.y
 
 func apply_marching_squares(map: Dictionary, cases: Dictionary):
 	print(cases)
